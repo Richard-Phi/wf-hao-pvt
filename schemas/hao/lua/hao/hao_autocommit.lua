baba5339655d904bc50aedeb9779a02dd9331d5e
Hertz Hwang <hertz@26hz.com.cn>
2025-07-20 09:45:41 +0800

    2 updates:
    - fix(div): 「𬾘𬽩𬽬𬾏𬾑𬾙𬾵𬾹𬿬」。
    
    - fix(lua): hao_autocommit

--- schemas/hao/lua/hao/hao_autocommit.lua
+++ schemas/hao/lua/hao/hao_autocommit.lua
@@ -146,8 +146,17 @@ local function func(key_event, env)
         end
     end
     
+    if current_char:match("[0-9]") then
+        return kNoop
+    end
+
     -- 检查是否是第五个编码
     if #input == 4 then
+        -- 如果第五码是数字键，则作为选重功能，不自动上屏
+        if current_char:match("[0-9 ]") then
+            return kNoop
+        end
+        
         -- 先让按键输入
         context:push_input(current_char)
         
